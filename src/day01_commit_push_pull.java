9a08caa9e2defe172cb360c99f9b131dded1ed04
AJ <jhaidriamin@gmail.com>
2026-03-22 12:02:10 -0500

    add print massage for Git practice

--- src/day01_commit_push_pull.java
+++ src/day01_commit_push_pull.java
@@ -3,5 +3,6 @@ public class day01_commit_push_pull {
     public static void main(String[] args) {
 
         System.out.println("Hello , Git!");
+        System.out.println("First change :practicing Git commit !");
     }
 }
